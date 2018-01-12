13512aa0761d28061903901a1dad3d4b5138172b
Helio Machado <crushedice2000@gmail.com>
2018-01-12 20:43:52 +0100

    Update botella.py

--- botella.py
+++ botella.py
@@ -94,7 +94,7 @@ def message(event):
     text = event["text"].casefold()
     result="No estoy segura de haberte entendido. Puedes quejarte ante <@U7EEV8AMQ> para que me reprograme. Si lo que quieres es responder a una pregunta que te he hecho, pulsa sobre la respuesta, que para eso soy interactiva. {}".format("¡Gracias!" if event["user"] == "U7G0C8L02" else "")
     if re.compile('[^a-z]*hola[^a-z]*(mundo[^a-z]*)?').match(text) is not None:
-        result = "¡Hola, mundo remoto!"
+        result = "¡Hola, mundo remoto 1!"
     if re.compile('[^a-z]*hola[^a-z]*@?botella[^a-z]*').match(text) is not None:
         name = slack_client.api_call("users.info", user=event["user"])["user"]["real_name"]
         result = "¡Hola, {}!".format(name)
