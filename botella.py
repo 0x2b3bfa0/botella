ef908b70f170695566d2ccabffcec911e0cbcb57
Helio Machado <casa@iMac-de-Helio.lan>
2018-01-13 15:31:37 +0100

    More silly dfebug

--- botella.py
+++ botella.py
@@ -156,12 +156,16 @@ def ask(question, answer=None):
         }
         for index, option in enumerate(question["options"])
     ]
-    print("DEBUG", question, flush=True, file=sys.stderr)
     if answer is not None:
         correct = answer == question["answer"][0]
+        print("DEBUG", question, flush=True, file=sys.stderr)
         if "prepend" in question:
+            print("DEBUG", "YESS", flush=True, file=sys.stderr)
+
             right, wrong = question["prepend"]
         else:
+            print("DEBUG", "NOO", flush=True, file=sys.stderr)
+
             right, wrong = "Efectivamente", "No"
         for index, _ in enumerate(data["actions"]):
             data["actions"][index]["style"] = "primary" if index == question["answer"][0] else "danger"
